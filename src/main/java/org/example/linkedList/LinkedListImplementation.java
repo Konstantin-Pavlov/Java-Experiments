bd06a4be25737bc9fb9212492b089d0ee0d9724a
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-17 22:39:19 +0600

    added new methods - pop, print, isEmpty, size, get, add

--- src/main/java/org/example/linkedList/LinkedListImplementation.java
+++ src/main/java/org/example/linkedList/LinkedListImplementation.java
@@ -1,4 +1,79 @@
 package org.example.linkedList;
 
-public class LinkedListImplementation {
+import MyExceptions.CustomException;
+
+public class LinkedListImplementation<T> {
+    private int size;
+    private Node<T> head;
+
+    public LinkedListImplementation() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public T get(int index) throws CustomException {
+        if (isEmpty()) {
+            throw new CustomException("array is empty");
+        }
+        if (index < 0) {
+            throw new CustomException("negative index");
+        }
+        if (index > size()) {
+            throw new CustomException("index more than array size");
+        }
+        Node<T> current = this.head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return current.getData();
+    }
+
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+
+        if (head == null) {
+            this.head = newNode;
+        } else {
+            Node<T> current = this.head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+        this.size++;
+    }
+
+    public boolean isEmpty(){
+        return this.head == null;
+    }
+
+    public T pop() throws CustomException {
+        if (isEmpty()) {
+            throw new CustomException("array is empty");
+        }
+        Node<T> current = this.head;
+        T popValue;
+        while (current.getNext().getNext() != null) {
+            current = current.getNext();
+        }
+        popValue = current.getNext().getData();
+        current.setNext(null);
+        return popValue;
+    }
+
+    public void print(){
+        if (isEmpty()) {
+            System.err.println("list is empty");
+        }
+        Node<T> current = this.head;
+        while (current != null) {
+            System.out.print(current.getData() + " ");
+            current = current.getNext();
+        }
+        System.out.println();
+    }
 }
