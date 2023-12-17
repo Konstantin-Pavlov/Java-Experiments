688ab8632c8c178526d80798983a17b55e20d521
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-17 22:40:36 +0600

    created class ListRunner for experiments with LinkedListImplementation

---
+++ src/main/java/org/example/linkedList/ListRunner.java
@@ -0,0 +1,15 @@
+package org.example.linkedList;
+
+import MyExceptions.CustomException;
+
+import java.util.stream.IntStream;
+
+public class ListRunner {
+    public static void main(String[] args) throws CustomException {
+        LinkedListImplementation<Integer> list = new LinkedListImplementation<>();
+        IntStream.range(0, 10).forEach(list::add);
+        list.print();
+        System.out.println(list.pop());
+        list.print();
+    }
+}
