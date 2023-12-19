bd29155b0b0e95f6b1b7c86083db581c416e7f14
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-19 14:10:43 +0600

    updated

--- src/main/java/org/example/linkedList/ListRunner.java
+++ src/main/java/org/example/linkedList/ListRunner.java
@@ -9,5 +9,26 @@ public class ListRunner {
         LinkedListImplementation<Integer> list = new LinkedListImplementation<>();
         IntStream.range(0, 10).forEach(list::add);
         System.out.println(list);
+
+        System.out.println(list.find(null));
+
+//        list.add(null);
+//        list.add(null);
+//        list.add(null);
+//        System.out.println(list);
+//
+//        list.add(65);
+//
+//        System.out.println(list);
+//
+//        IntStream.range(9, 16).forEach(element -> {
+//            try {
+//                System.out.println(list.get(element));
+//            } catch (CustomException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        });
+
+
     }
 }
