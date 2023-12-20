24975797015314c63c416d474cdbb5f75c97b887
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-20 16:14:26 +0600

    refactoring

--- src/main/java/org/example/fuctionalInterfaceExample/cat/Printer.java
+++ src/main/java/org/example/fuctionalInterfaceExample/cat/Printer.java
@@ -10,18 +10,18 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public final class Printer {
-    private static final void printHeader() {
-        System.out.println(String.format("%1$10.10s | %1$3.3s | %1$-10.10s | %1$-10.10s |", "--------------"));
-        System.out.println(String.format("%10.10s | %3.3s | %-10.10s | %-10.10s |", "Name", "Age", "Breed", "Color"));
-        System.out.println(String.format("%1$10.10s | %1$3.3s | %1$-10.10s | %1$-10.10s |", "--------------"));
+    private static void printHeader() {
+        System.out.printf("%1$10.10s | %1$3.3s | %1$-10.10s | %1$-10.10s |%n", "--------------");
+        System.out.printf("%10.10s | %3.3s | %-10.10s | %-10.10s |%n", "Name", "Age", "Breed", "Color");
+        System.out.printf("%1$10.10s | %1$3.3s | %1$-10.10s | %1$-10.10s |%n", "--------------");
     }
 
-    private static final void printCat(Cat cat) {
-        System.out.println(String.format("%10.10s | %3.3s | %-10.10s | %-10.10s |", cat.getName(), cat.getAge(),
-                cat.getBreed(), cat.getColor()));
+    private static void printCat(Cat cat) {
+        System.out.printf("%10.10s | %3.3s | %-10.10s | %-10.10s |%n", cat.getName(), cat.getAge(),
+                cat.getBreed(), cat.getColor());
     }
 
-    public static final void print(List<Cat> cats) {
+    public static void print(List<Cat> cats) {
         printHeader();
         cats.forEach(Printer::printCat);
         System.out.println();
