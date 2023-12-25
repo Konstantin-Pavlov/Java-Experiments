42471e790d20c6d5ef94dc3d47ed0c12af2ab8d4
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-25 07:02:59 +0600

    added Printer2DArray util class

---
+++ src/main/java/utils/Printer2DArray.java
@@ -0,0 +1,90 @@
+package utils;
+
+import java.util.Arrays;
+
+public class Printer2DArray {
+    private Printer2DArray() {
+    }
+
+
+    // Generic method accepting 2D arrays of non-primitive values
+    public static <T> void print(T[][] arr) {
+        Arrays.stream(arr)
+                .forEach(row -> {
+                    Arrays.stream(row)
+                            .forEach(element -> System.out.print(element + " "));
+                    System.out.println();
+                });
+        System.out.println();
+    }
+
+
+    // Method for byte 2D arrays
+    public static void print(byte[][] arr) {
+        Arrays.stream(arr)
+                .forEach(row -> {
+                    for (byte b : row) {
+                        System.out.print(b + " ");
+                    }
+                    System.out.println();
+                });
+        System.out.println();
+    }
+
+    // Method for int 2D arrays
+    public static void print(int[][] arr) {
+        Arrays.stream(arr)
+                .forEach(row -> {
+                    Arrays.stream(row)
+                            .forEach(element -> System.out.print(element + " "));
+                    System.out.println();
+                });
+        System.out.println();
+    }
+
+    // Method for long 2D arrays
+    public static void print(long[][] arr) {
+        Arrays.stream(arr)
+                .forEach(row -> {
+                    Arrays.stream(row)
+                            .forEach(element -> System.out.print(element + " "));
+                    System.out.println();
+                });
+        System.out.println();
+    }
+
+    // Method for float 2D arrays
+    public static void print(float[][] arr) {
+        Arrays.stream(arr)
+                .forEach(row -> {
+                    for (float f : row) {
+                        System.out.print(f + " ");
+                    }
+                    System.out.println();
+                });
+        System.out.println();
+    }
+
+    // Method for double 2D arrays
+    public static void print(double[][] arr) {
+        Arrays.stream(arr)
+                .forEach(row -> {
+                    Arrays.stream(row)
+                            .forEach(element -> System.out.print(element + " "));
+                    System.out.println();
+                });
+        System.out.println();
+    }
+
+    // Method for char 2D arrays
+    public static void print(char[][] arr) {
+        Arrays.stream(arr)
+                .forEach(row -> {
+                    for (char c : row) {
+                        System.out.print(c + " ");
+                    }
+                    System.out.println();
+                });
+        System.out.println();
+    }
+}
