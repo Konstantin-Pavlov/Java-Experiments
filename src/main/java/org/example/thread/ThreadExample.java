8eec8b257b31b6e7bd70aaa1770e5124dd6e113f
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-25 17:07:05 +0600

    added runnable example

--- src/main/java/org/example/thread/ThreadExample.java
+++ src/main/java/org/example/thread/ThreadExample.java
@@ -4,16 +4,29 @@ import java.util.Random;
 
 public class ThreadExample {
     public static void main(String[] args) {
+        withMyThreadExample();
+
+        withMyRunnableExample();
+
+        System.out.println("main method");
+
+    }
+
+    private static void withMyRunnableExample() {
+        Thread thread1 = new Thread(new Runner());
+        Thread thread2 = new Thread(new Runner());
+        thread1.start();
+        thread2.start();
+    }
+
+    private static void withMyThreadExample() {
         MyThread thread1 = new MyThread();
         MyThread thread2 = new MyThread();
         thread1.start();
         thread2.start();
     }
-}
 
-class MyThread extends Thread {
-    @Override
-    public void run() {
+    public static void runningForLoop(String name) {
         for (int i = 0; i < 10; i++) {
             // 50% that thread sleeps
             if (new Random().nextBoolean()) {
@@ -23,7 +36,23 @@ class MyThread extends Thread {
                     throw new RuntimeException(e);
                 }
             }
-            System.out.println(i);
+            System.out.println(i + ": " + name);
         }
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("MyThread starts");
+        ThreadExample.runningForLoop("MyThread");
+    }
+}
+
+class Runner implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Runner starts");
+        ThreadExample.runningForLoop("Runner");
     }
 }
