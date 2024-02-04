211b6cb1e982c64feeb7dcc26fed3c56a48a57b2
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-02-04 20:25:57 +0600

    add new methods to read points from the console

--- src/main/java/org/example/pointsComparator/PointRunner.java
+++ src/main/java/org/example/pointsComparator/PointRunner.java
@@ -1,22 +1,48 @@
 package org.example.pointsComparator;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class PointRunner {
     public static void main(String[] args) {
-        Point[] students = new Point[]{
+        Point[] points = getTwoPoints();
+        Point.showTheComparisonResultOfTwoPoints(points);
+
+
+//        withoutUserInput();
+//        withUserInput();
+    }
+
+    @NotNull
+    private static Point[] getTwoPoints() {
+        Scanner scanner = new Scanner(System.in);
+        int numPoints = 2;
+        Point[] points = new Point[numPoints];
+        IntStream.range(0, numPoints)
+                .forEach(i -> {
+                    String[] coordinates = scanner.nextLine().split(" ");
+                    double x = Double.parseDouble(coordinates[0]);
+                    double y = Double.parseDouble(coordinates[1]);
+                    points[i] = new Point(x, y);
+                });
+        scanner.close();
+        return points;
+    }
+
+
+    private static void withoutUserInput() {
+        Point[] points = new Point[]{
                 new Point(1, 2),
                 new Point(3, 7),
                 new Point(-2, 3),
                 new Point(1, 1),
                 new Point(3, 5)
         };
-        Arrays.sort(students);
-        Arrays.stream(students).forEach(System.out::println);
-
-//        withUserInput();
+        Arrays.sort(points);
+        Arrays.stream(points).forEach(System.out::println);
     }
 
     private static void withUserInput() {
