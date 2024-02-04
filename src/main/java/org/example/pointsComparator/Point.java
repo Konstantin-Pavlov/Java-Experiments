6fddc6e1a5ca43ab0d98d28de8ae233c784be42a
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-02-04 20:26:51 +0600

    add new methods, add overload constructor for doubles

--- src/main/java/org/example/pointsComparator/Point.java
+++ src/main/java/org/example/pointsComparator/Point.java
@@ -9,10 +9,15 @@ and display the sorted list of points on the screen in the format:
  */
 
 class Point implements Comparable<Point> {
-    private final int x;
-    private final int y;
+    private final double x;
+    private final double y;
 
     public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
@@ -24,8 +29,28 @@ class Point implements Comparable<Point> {
         return Double.compare(distance1, distance2);
     }
 
+    public static void showTheComparisonResultOfTwoPoints(Point[] points) {
+        if (points == null) {
+            System.err.println("points array is null");
+            return;
+        }
+        if (points.length != 2) {
+            System.err.println("points array length should be 2");
+            return;
+        }
+
+        int result = points[0].compareTo(points[1]);
+        if (result < 0) {
+            System.out.println("Первая точка ближе");
+        } else if (result > 0) {
+            System.out.println("Вторая точка ближе");
+        } else {
+            System.out.println("Точки на равных расстояниях");
+        }
+    }
+
     @Override
     public String toString() {
-        return String.format("(%d; %d)", x, y);
+        return String.format("(%.2f; %.2f)", x, y);
     }
 }
