bb131305c962b281879b4a4ca455d2524f555ed5
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-03-06 15:46:55 +0600

    add example of ExecutorService class and calculating how long threads take to finish their work

---
+++ src/main/java/org/example/thread/ExecutorServiceExample.java
@@ -0,0 +1,55 @@
+package org.example.thread;
+
+import util.ConsoleColors;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class ExecutorServiceExample {
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        int taskCount = 8;
+        submitTaskInto(pool, taskCount);
+        pool.shutdown();
+        measure(pool);
+        System.out.println("*".repeat(42));
+    }
+
+    static void submitTaskInto(ExecutorService pool, int taskCount) {
+        System.out.println(ConsoleColors.ANSI_GREEN_BACKGROUND + "creating tasks" + ConsoleColors.RESET);
+        IntStream.rangeClosed(1, taskCount)
+                .mapToObj(ExecutorServiceExample::makeTask)
+                .forEach(pool::submit);
+    }
+
+    static Runnable makeTask(int taskId) {
+        int temp = new Random().nextInt(20000) + 10000;
+        int taskTime = (int) TimeUnit.MILLISECONDS.toSeconds(temp);
+        return () -> heavyTask(taskId, taskTime);
+    }
+
+    static void heavyTask(int taskId, int taskTime) {
+        System.out.printf(ConsoleColors.ANSI_BLUE + "Task %d will take %d seconds%n" + ConsoleColors.RESET, taskId, taskTime);
+        try {
+            Thread.sleep(taskTime * 1000L);
+            System.out.printf(ConsoleColors.GREEN + "Task %d finished in %d seconds%n" + ConsoleColors.RESET, taskId, taskTime);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void measure(ExecutorService pool) {
+        long start = System.nanoTime();
+        try {
+            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+        long delta = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start);
+        System.out.printf(ConsoleColors.PURPLE_BOLD_BRIGHT + "%nProgram execution took %d seconds%n" + ConsoleColors.RESET, delta);
+    }
+}
