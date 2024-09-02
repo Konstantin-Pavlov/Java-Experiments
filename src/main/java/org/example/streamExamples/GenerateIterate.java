7c178bfa21a9ea6b8f704d2be6349f4937590e63
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-09-02 16:44:13 +0600

    fix

--- src/main/java/org/example/streamExamples/GenerateIterate.java
+++ src/main/java/org/example/streamExamples/GenerateIterate.java
@@ -1,8 +1,6 @@
 package org.example.streamExamples;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Stream;
 
@@ -12,6 +10,7 @@ public class GenerateIterate {
 
         Stream<Integer> randNums = Stream.generate(() -> random.nextInt(25)).limit(25);
         Stream<Integer> nums = Stream.iterate(0, x -> x + 1).limit(10);
+
         randNums.forEach(n -> System.out.print(n + " "));
         System.out.println();
         nums.forEach(n -> System.out.print(n + " "));
@@ -19,13 +18,8 @@ public class GenerateIterate {
         System.out.println();
 
         List<String> words = List.of("wew", "like", "woof", "wow", "lol");
-        Optional[] startsWithW = words.stream().filter(w -> w.startsWith("w")).map(Optional::of).toArray(Optional[]::new);
-        Arrays.stream(startsWithW).forEach(n -> {
-            if(n.isPresent()){
-                System.out.print(n.get() + " ");
-            }
-//            System.out.print(n.orElse("not present") + " ");
-        });
+        List<String> startsWithW = words.stream().filter(w -> w.startsWith("w")).toList();
+        startsWithW.forEach(n -> System.out.print(n + " "));
 
     }
 }
--- src/main/java/org/example/streamExamples/IntStreamGenerateRandom.java
+++ src/main/java/org/example/streamExamples/IntStreamGenerateRandom.java
@@ -3,15 +3,13 @@ package org.example.streamExamples;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Random;
-import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class IntStreamGenerateRandom {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Locale.setDefault(Locale.US);
-        int n = scanner.nextInt();
-        int seed = scanner.nextInt();
+        int n = 10;
+        int seed = 42;
         Random random = new Random(seed);
         int[] nums = IntStream
                 .generate(() -> random.nextInt(-5, 6))
@@ -23,7 +21,5 @@ public class IntStreamGenerateRandom {
                 Arrays.stream(nums).filter(num -> num >= 0).sum(),
                 Arrays.stream(nums).mapToDouble(x -> x).filter(num -> num < 0).reduce((x, y) -> x * y).orElse(0.0)
         );
-
-        scanner.close();
     }
 }
